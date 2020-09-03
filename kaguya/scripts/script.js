a31527442b9c64d05b27ed14255b1a279bee571b
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-09-03 21:56:57 +0900

    ページ遷移時のアニメーションを実装。

--- kaguya/scripts/script.js
+++ kaguya/scripts/script.js
@@ -1,16 +1,21 @@
 'use strict';
 
 $(function () {
+  // ページ遷移時のアニメーション
+  $('main').hide().fadeIn(500);
+
+  // ここからハンバーガーメニュー
   $('.toggle-btn').click(function () {
     $(this).toggleClass('toggle-open');
     $('.mobile-menu').toggleClass('active');
   });
-
   $('.mobile-menu').click(function () {
     $(this).toggleClass('active');
     $('.toggle-btn').toggleClass('toggle-open');
   });
+  // ここまでハンバーガーメニュー
   
+  // pc版NEWSページの画像ホバー時のアニメーション
   if (window.matchMedia('(min-width: 376px)').matches) {
     $('.update').hide()
     $('.news-item').mouseenter(
@@ -23,8 +28,10 @@ $(function () {
     );
   }
 
+  // 「SEND」をクリックすると「TRANSMISSION COMPLETED」にボタン文言を変更する
   $('.submission-btn').click(function () {
     $(this).val("TRANSMISSION COMPLETED");
     $(this).addClass('animation');    
   })
+
 });
